5cbdf57884b71121d491748efcce9797155e35f2
nabeel1 <nabeel.nasar1@gmail.com>
2022-05-13 14:20:26 +0500

    First Commit

---
+++ api/routes/index.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var router = express.Router();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const db = mysql.createPool({
+host: "localhost",
+user: "root",
+password: "",
+database: "moviedb",
+});
+
+/* GET result from db */
+router.get('/api/get', function(req, res) {
+  const mysql_get = "SELECT * FROM movie_reviews";
+  db.query(mysql_get,(err,result)=>{
+    res.send(result);
+  });
+
+});
+
+/* Insert into DB. */
+router.post('/api/insert', function(req, res) {
+  const movieName = req.body.movieName;
+  const movieReview = req.body.movieReview;
+
+  const mysql_insert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
+  db.query(mysql_insert,[movieName,movieReview], (err,result) =>{
+    console.log(result);
+  });
+
+});
+
+/* Delete result from db */
+router.delete("/api/del/:movieName", function(req, res) {
+  const Name = req.params.movieName;
+  const mysql_del = "DELETE FROM movie_reviews WHERE movieName=?";
+  db.query(mysql_del,Name,(err,result)=>{
+      if(err) console.log(err);
+  });
+
+});
+
+/* Update result from db */
+router.put("/api/update", function(req, res) {
+  const name = req.body.movieName;
+  const review = req.body.movieReview;
+  const mysql_update = "UPDATE movie_reviews SET movieReview=? WHERE movieName=?";
+  db.query(mysql_update,[review,name],(err,result)=>{
+      if(err) console.log(err);
+  });
+
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+module.exports = router;
---
+++ client/src/App.js
@@ -0,0 +1,76 @@
+import React, {useState} from 'react';
+import './App.css';
+import { useEffect } from 'react';
+import axios, { Axios } from 'axios';
+
+function App() {
+  
+    const [movieName, setmovieName] = useState("");
+    const [movieReview, setmovieReview] = useState("");
+    const [movieReviewList, setMovieReviewList] = useState ([]);
+    const [NewmovieReview, setNewmovieReview] = useState("");
+
+    useEffect(() =>{
+      axios.get("http://127.0.0.1:9000/api/get").then((Response) => {
+        setMovieReviewList(Response.data);
+      });
+    },[]);
+
+    const deleteMovie = (movie) => {     
+        axios.delete(`http://127.0.0.1:9000/api/del/${movie}`);
+
+    };
+
+    const updateMovie = (movie) => {     
+      axios.put('http://127.0.0.1:9000/api/update',{
+        movieName: movie,
+        movieReview: NewmovieReview
+      });
+      
+      setNewmovieReview("");
+      alert("Update Active"+movie);
+  };
+
+    const submitReview = () => {
+      axios.post('http://127.0.0.1:9000/api/insert', {
+        movieName: movieName, movieReview:movieReview
+      }).then(()=>{
+          alert("Successful Insert");
+      });
+
+      setMovieReviewList([...movieReviewList, {movieName: movieName, movieReview: movieReview}]);
+
+    };
+
+  return (
+    <div className="App">
+      <h1>Movie Management System</h1>
+      <br />
+      <div className="form">
+        <label>Movie Name:</label>
+        <input type="text" name="movieName" onChange={(e) => {setmovieName(e.target.value)}} />
+        <label>Movie Review:</label>
+        <input type="text" name="review" onChange={(e) => {setmovieReview(e.target.value)}} />
+        <button onClick={submitReview}>Submit</button>
+      </div>
+        <br />
+        
+        
+        {movieReviewList.map((val) => {
+          return (
+            <div className="mycard">
+            <h1>{val.movieName} </h1> 
+            <p>{val.movieReview}</p>
+
+              <button type='button' onClick={()=>{deleteMovie(val.movieName)}}>Delete</button>
+              <input id="myInput" type="text" onChange={(e) => {setNewmovieReview(e.target.value)}} />
+              <button type='button' onClick={()=>{updateMovie(val.movieName)}} >Update</button>
+
+            </div>
+            );
+        })}
+    </div>
+  );
+}
+
+export default App;
